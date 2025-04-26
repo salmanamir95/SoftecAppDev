d46ce463ad8bdb631b303dab29eed47a624d6281
mmsa1 <mmsa155j3@example.com>
2025-04-26 13:30:01 +0500

    Task Ongoing

---
+++ app/src/main/java/com/example/personalizedailifeassistant/PersonalizedDashboard/Tasks/Task.kt
@@ -0,0 +1,7 @@
+package com.example.personalizedailifeassistant.PersonalizedDashboard.Tasks
+
+data class Task(val title: String,
+                val description: String = "",
+                val dueDate: String = "",
+                val priority: Int = 0,
+                val category: String = "") {}
